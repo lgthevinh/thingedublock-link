c868382f1cbd6953ac0f7373345f20589d40c77b
lgthvinh <everwellmax@gmail.com>
2024-11-11 20:57:20 +0700

    update repo location

--- script/download-tools.js
+++ script/download-tools.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 const fs = require('fs');
 
-const user = 'lgthevinh';
+const user = 'thingedublock';
 const repo = 'thingedublock-tools';
 const outputdir = path.resolve('./tools');
 const leaveZipped = false;
